d19320f8221e575a5336a183b931fdb72f710a24
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 14:42:40 -0600

    ejercicio sobre control if

--- src/main/java/guia1poo1/estructurascontrol/ControlIf2.java
+++ src/main/java/guia1poo1/estructurascontrol/ControlIf2.java
@@ -8,9 +8,33 @@ package guia1poo1.estructurascontrol;
  *
  * @author LENOVO
  */
+//Import que permite usar GUI
+import javax.swing.JOptionPane;
 public class ControlIf2 {
     public static void main(String[] args) {
-         
+         //Variable string vacia
+        String v1 = "";
+        //Por medio de un cuadro de dialogo se pide al usuario 
+        //Escribir un valor y quedara guardado en v1
+        v1=JOptionPane.showInputDialog("Ingrese un valor");
+        
+        //Si la variable es nula es porque el usuario apreto el boton cancelar
+        if(v1 == null)
+        {
+            JOptionPane.showMessageDialog(null,"Apretaste cancelar");
+        }
+        //Si no apreto el boton cancelar entonces...
+        else{
+            //Si la variable esta vacia entonces que imprima el siguiente mensaje
+            if(v1.equals(""))
+            {
+                JOptionPane.showMessageDialog(null, "No ingresaste nada");
+            }
+            //Si no el usuario digito un numero diferente de 0
+            else{
+                JOptionPane.showMessageDialog(null, "El valor de V1 es: " +v1);
+            }
+        }
     
     }
 }
