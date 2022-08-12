45edb7fc8373203fec0438d27698e3f55e4785ca
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 14:44:22 -0600

    Se agrego nueva clase java controlif3

---
+++ src/main/java/guia1poo1/estructurascontrol/Controlif3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia1poo1.estructurascontrol;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Controlif3 {
+    public static void main(String[] args) {
+    
+    }
+}
