c6188f6efd213725e460bd7590356a88ba945dbe
CARLA MARIA ARTIGA NAVARRETE <202000791@ujmd.edu.sv>
2022-08-12 15:08:39 -0600

    Resolucion de ejercicio else if

--- src/main/java/guia1poo1/estructurascontrol/Controlif3.java
+++ src/main/java/guia1poo1/estructurascontrol/Controlif3.java
@@ -8,8 +8,25 @@ package guia1poo1.estructurascontrol;
  *
  * @author LENOVO
  */
+import javax.swing.JOptionPane;
 public class Controlif3 {
     public static void main(String[] args) {
-    
+       
+        //Variable
+        int result = JOptionPane.showConfirmDialog(null,"Replace existing selection? ");
+        System.out.println("El numero devuelto es: "+result);
+        
+        if(result == JOptionPane.YES_OPTION){
+            System.out.println("Yes");
+        }
+        else if(result == JOptionPane.NO_OPTION){
+                System.out.println("No");
+            }
+        else if(result == JOptionPane.CANCEL_OPTION){
+                System.out.println("Cancel");
+        }
+        else if (result == JOptionPane.CLOSED_OPTION){
+                 System.out.println("Closed");
+        }
     }
 }
